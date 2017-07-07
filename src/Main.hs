044447b208f8a905d75f9667cc136b680fc62db1
Sebastian Lopez-Cot <slcot@mit.edu>
2017-07-07 03:39:47 -0400

    Working on menu

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,79 @@
+-- module Main (main) where
+
+-- import Graphics.Gloss.Interface.Pure.Game
+--     ( Color
+--     , Display(InWindow)
+--     , Event(EventKey)
+--     , Key(Char)
+--     , KeyState(Down, Up)
+--     , Picture
+--     , circleSolid
+--     , makeColor
+--     , play
+--     , translate
+--     )
+
+-- data Player = Player (Float, Float) (Float, Float) Float
+
+-- main :: IO ()
+-- main = play display background framerate initial render handle step
+
+-- display :: Display
+-- display = InWindow "game" (800, 600) (800, 600)
+
+-- background :: Color
+-- background = makeColor 1 1 1 1
+
+-- framerate :: Int
+-- framerate = 60
+
+-- initial :: Player
+-- initial = Player (0, 0) (0, 0) 20
+
+-- render :: Player -> Picture
+-- render (Player (x, y) _ z) = translate x y $ circleSolid z
+
+-- handle :: Event -> Player -> Player
+-- handle (EventKey (Char 'w') Down _ _) (Player p (dx, dy) s) = Player p (dx, dy + 80) s
+-- handle (EventKey (Char 'w') Up _ _) (Player p (dx, dy) s) = Player p (dx, dy - 80) s
+-- handle (EventKey (Char 'a') Down _ _) (Player p (dx, dy) s) = Player p (dx - 80, dy) s
+-- handle (EventKey (Char 'a') Up _ _) (Player p (dx, dy) s) = Player p (dx + 80, dy) s
+-- handle (EventKey (Char 's') Down _ _) (Player p (dx, dy) s) = Player p (dx, dy - 80) s
+-- handle (EventKey (Char 's') Up _ _) (Player p (dx, dy) s) = Player p (dx, dy + 80) s
+-- handle (EventKey (Char 'd') Down _ _) (Player p (dx, dy) s) = Player p (dx + 80, dy) s
+-- handle (EventKey (Char 'd') Up _ _) (Player p (dx, dy) s) = Player p (dx - 80, dy) s
+-- handle (EventKey (Char 'q') Down _ _) (Player p v s) = Player p v (s - 5)
+-- handle (EventKey (Char 'e') Down _ _) (Player p v s) = Player p v (s + 5)
+-- handle _ w = w
+
+-- step :: Float -> Player -> Player
+-- step t (Player (x, y) (dx, dy) s) = Player (x + t * dx, y + t * dy) (dx, dy) s
+
 module Main (main) where
 
 import Graphics.Gloss.Interface.Pure.Game
     ( Color
     , Display(InWindow)
     , Event(EventKey)
-    , Key(Char)
+    , Key(Char, SpecialKey)
+    , SpecialKey(KeyUp, KeyDown)
     , KeyState(Down, Up)
-    , Picture
-    , circleSolid
+    , Picture (..)
+    , text
+    , circle
     , makeColor
     , play
     , translate
+    , scale
     )
 
-data Player = Player (Float, Float) (Float, Float) Float
+data MenuList = MenuList [String] Int
 
 main :: IO ()
 main = play display background framerate initial render handle step
 
 display :: Display
-display = InWindow "game" (800, 600) (800, 600)
+display = InWindow "Grand Theft Hammock" (800, 600) (800, 600)
 
 background :: Color
 background = makeColor 1 1 1 1
@@ -27,24 +81,31 @@ background = makeColor 1 1 1 1
 framerate :: Int
 framerate = 60
 
-initial :: Player
-initial = Player (0, 0) (0, 0) 20
+initial :: MenuList
+initial = MenuList ["New Game", "Continue", "Options", "Quit"] 0
 
-render :: Player -> Picture
-render (Player (x, y) _ z) = translate x y $ circleSolid z
+listItemVerticalPadding :: Float
+listItemVerticalPadding = 55
 
-handle :: Event -> Player -> Player
-handle (EventKey (Char 'w') Down _ _) (Player p (dx, dy) s) = Player p (dx, dy + 80) s
-handle (EventKey (Char 'w') Up _ _) (Player p (dx, dy) s) = Player p (dx, dy - 80) s
-handle (EventKey (Char 'a') Down _ _) (Player p (dx, dy) s) = Player p (dx - 80, dy) s
-handle (EventKey (Char 'a') Up _ _) (Player p (dx, dy) s) = Player p (dx + 80, dy) s
-handle (EventKey (Char 's') Down _ _) (Player p (dx, dy) s) = Player p (dx, dy - 80) s
-handle (EventKey (Char 's') Up _ _) (Player p (dx, dy) s) = Player p (dx, dy + 80) s
-handle (EventKey (Char 'd') Down _ _) (Player p (dx, dy) s) = Player p (dx + 80, dy) s
-handle (EventKey (Char 'd') Up _ _) (Player p (dx, dy) s) = Player p (dx - 80, dy) s
-handle (EventKey (Char 'q') Down _ _) (Player p v s) = Player p v (s - 5)
-handle (EventKey (Char 'e') Down _ _) (Player p v s) = Player p v (s + 5)
+renderListItem :: (String, Int) -> Picture
+renderListItem (item, i) = let verticalPadding = listItemVerticalPadding
+                            in 
+                            translate (-300) (-verticalPadding*(fromIntegral i)) 
+                            $ scale 0.33 0.33 
+                            $ text item
+
+renderListSelector :: Int -> Picture
+renderListSelector i = translate (-320) (-listItemVerticalPadding*(fromIntegral i)+14)
+                            $ circle 10
+
+render :: MenuList -> Picture
+render (MenuList items s) = Pictures ((renderListItem <$> zip items [0..(length items)-1]) ++ [renderListSelector s])
+
+handle :: Event -> MenuList -> MenuList
+handle (EventKey (SpecialKey KeyDown) Down _ _) (MenuList items s) = (MenuList items ((s+1) `mod` (length items)))
+handle (EventKey (SpecialKey KeyUp) Down _ _) (MenuList items s) = (MenuList items ((s-1) `mod` (length items)))
 handle _ w = w
 
-step :: Float -> Player -> Player
-step t (Player (x, y) (dx, dy) s) = Player (x + t * dx, y + t * dy) (dx, dy) s
+step :: Float -> MenuList -> MenuList
+step t w = w
+
